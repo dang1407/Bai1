ef5c8981d1fad61c139903984d0772f73d577312
Minh Dang <dang14072k2@gmail.com>
2024-04-14 23:31:12 +0700

    Bai 1

---
+++ Assets/BallSpawnerScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawnerScript : MonoBehaviour
+{
+    // Prefab quả bóng đỏ
+    public GameObject redBallPrefab;
+
+    // Prefab quả bóng xanh lục
+    public GameObject greenBallPrefab;
+
+    // Prefab quả bóng xanh dương
+    public GameObject blueBallPrefab;
+
+    public int[] ballCount = new int[3];
+
+    // Tốc độ sinh ra quả bóng (quả bóng/giây)
+    public float spawnRate = 10f;
+
+    // Thời gian chờ trước khi bắt đầu sinh ra quả bóng
+    public float startDelay = 0f;
+
+    private float lastSpawnTime = 0f;
+
+    void Start()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            SpawnBall(i);
+            ballCount[i] = 1;
+        }
+    }
+
+    // Cập nhật mỗi khung hình
+    void Update()
+    {
+        if (Time.time - lastSpawnTime > spawnRate)
+        {
+            SpawnBall(Random.Range(0, 3));
+            lastSpawnTime = Time.time;
+        }
+        for(int i = 0;i < 3;i++) 
+        {
+            if(ballCount[i] < 1)
+            {
+                SpawnBall(i);
+            }
+        }
+    }
+
+    // Hàm tạo quả bóng
+    public void SpawnBall(int ballType)
+    {
+        if (ballCount[ballType] < 5)
+        {
+            // Tạo quả bóng
+
+            Vector3 randomPosition = new Vector3(Random.Range(-25, -15), 1.5f, Random.Range(-30, -20));
+
+            switch (ballType)
+            {
+                case 0:
+                    Instantiate(redBallPrefab, randomPosition, Quaternion.identity);
+                    break;
+                case 1:
+                    Instantiate(greenBallPrefab, randomPosition, Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(blueBallPrefab, randomPosition, Quaternion.identity);
+                    break;
+            }
+            ballCount[ballType]++;
+        }
+    }
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerScript : MonoBehaviour
+{
+    public float speed = 10f;
+    public float horizontalInput;
+    public float verticalInput;
+    public string color;
+    public int score = 0;
+    public GameObject _ballSpawner;
+    public Text scoreText;
+    public Color playerColor; 
+
+    void Start()
+    {
+        Renderer playerRenderer = GetComponent<Renderer>();
+        playerColor = playerRenderer.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + score;
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(-Vector3.forward * Time.deltaTime * verticalInput * speed);
+        transform.Translate(-Vector3.right * Time.deltaTime * horizontalInput * speed);
+    }
+
+    // Phát hiện va chạm với quả bóng
+    void OnTriggerEnter(Collider other)
+    {
+        // Kiểm tra xem quả bóng có cùng màu với người chơi hay không
+        if (other.GetComponent<Renderer>().material.color == playerColor)
+        {
+                this.score += 1;
+            if (other.gameObject.tag == "RedBall")
+            {
+
+                _ballSpawner.GetComponent<BallSpawnerScript>().ballCount[0]--;
+                _ballSpawner.GetComponent<BallSpawnerScript>().SpawnBall(0);
+            }
+            if (other.gameObject.tag == "GreenBall")
+            {
+                _ballSpawner.GetComponent<BallSpawnerScript>().ballCount[1]--;
+                _ballSpawner.GetComponent<BallSpawnerScript>().SpawnBall(1);
+
+            }
+            if (other.gameObject.tag == "BlueBall")
+            {
+                _ballSpawner.GetComponent<BallSpawnerScript>().ballCount[2]--;
+                _ballSpawner.GetComponent<BallSpawnerScript>().SpawnBall(2);
+            }
+            // Ẩn quả bóng
+            Destroy(other.gameObject);
+            Renderer playerRenderer = GetComponent<Renderer>();
+            playerRenderer.material.color = GetRandomColor();   
+        }
+
+    }
+
+    public Color GetRandomColor()
+    {
+        int randomColor = Random.Range(0, 3);
+        switch (randomColor)
+        {
+            case 0:
+                return Color.red;
+            case 1:
+                return Color.green;
+            case 2:
+                return Color.blue;
+            default:
+                return Color.red;
+        }
+    }
+    
+}
